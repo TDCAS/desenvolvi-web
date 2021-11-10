852b3ef09f5118daba95f922608f526a41c95cb4
Allan <allanf333@gmail.com>
2021-11-10 19:47:45 -0300

    segundo comit

---
+++ DOM - HTML e JS/JS/script.js
@@ -0,0 +1,10 @@
+/* scripy JS
+    Autor: Allan fortes barbosa
+    Data:09/11/2021
+    Versão: 1.1
+    
+*/
+function mudarFuncao() {
+    document.getElementById("1").value = "novo Texto";
+    alert(document.getElementById("1").value);
+}
