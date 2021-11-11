6b6de43c622a36a96275edca07f57b6a0d61eb6b
Allan <allanf333@gmail.com>
2021-11-10 21:59:57 -0300

    terceiro commit aula do sor nao completa (olha aula 10)

--- DOM - HTML e JS/JS/script.js
+++ DOM - HTML e JS/JS/script.js
@@ -6,5 +6,35 @@
 */
 function mudarFuncao() {
     document.getElementById("1").value = "novo Texto";
-    alert(document.getElementById("1").value);
+    
 }
+// mudar valores com innerHTML
+function mudarTexto2() {
+    document.getElementById("texto2").innerHTML = "oloko";
+
+
+}
+
+// mudar css estilo 04
+//css modificado em js
+function mudarCss() {
+    //console.log(document.getElementById("texto3").innerHTML);
+    //document.getElementById("texto3").style.color = "blue";
+    var texto = document.getElementById("texto3");
+    texto.style.color = "white"
+    texto.style.fontSize = "1.5rem"
+}
+
+const matriz = []
+
+matriz[0] = "OK"
+
+function mudarTexto3 (id) {
+    console.log(id.innerHTML);
+
+    id.innerHTML = "Texto alterado por javaScript";
+}
+
+/*function mudarTexto3(valor1, valor2) {
+    console.log(valor1 + valor2);
+}*/
